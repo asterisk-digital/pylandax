8e0c885fa764a6f2caf2971724a651f4324f44f8
David Skoland <davidskoland@gmail.com>
2025-08-26 14:04:02 +0200

    format

--- tasks/get_linked_documents.py
+++ tasks/get_linked_documents.py
@@ -12,24 +12,24 @@ script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
 def main():
-    envpath = Path(script_dir, '..', '.env-dev')
+    envpath = Path(script_dir, "..", ".env-dev")
     dotenv.load_dotenv(envpath)
 
     conf = {
-        'url': os.getenv('LANDAX_URL'),
-        'credentials': {
-            'username': os.getenv('LANDAX_USERNAME'),
-            'password': os.getenv('LANDAX_PASSWORD'),
-            'client_id': os.getenv('LANDAX_CLIENT_ID'),
-            'client_secret': os.getenv('LANDAX_CLIENT_SECRET'),
-        }
+        "url": os.getenv("LANDAX_URL"),
+        "credentials": {
+            "username": os.getenv("LANDAX_USERNAME"),
+            "password": os.getenv("LANDAX_PASSWORD"),
+            "client_id": os.getenv("LANDAX_CLIENT_ID"),
+            "client_secret": os.getenv("LANDAX_CLIENT_SECRET"),
+        },
     }
-    client = pylandax.Client(conf['url'], conf['credentials'])
+    client = pylandax.Client(conf["url"], conf["credentials"])
 
-    linked_documents = client.get_linked_documents('INCIDENTS', 36)
+    linked_documents = client.get_linked_documents("INCIDENTS", 36)
 
     print(linked_documents)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,26 +11,23 @@ script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
 def test_basic():
-    confpath = Path(script_dir, 'mock_config.json')
+    confpath = Path(script_dir, "mock_config.json")
     with open(confpath) as file:
-        conf = json.loads(file.read())['landax']
+        conf = json.loads(file.read())["landax"]
 
     try:
-        client = pylandax.Client(conf['url'], conf['credentials'])
+        client = pylandax.Client(conf["url"], conf["credentials"])
     # Since the URL is bogus, this is what we expect
     except pylandax.LandaxAuthException:
         pass
 
 
 def test_generate_url():
-    base_url = 'https://test.landax.com'
-    params = {
-        '$test': 'test',
-        '$test2': 'test2'
-    }
+    base_url = "https://test.landax.com"
+    params = {"$test": "test", "$test2": "test2"}
 
     result = pylandax.Client.generate_url(base_url, params)
-    assert result == 'https://test.landax.com?%24test=test&%24test2=test2'
+    assert result == "https://test.landax.com?%24test=test&%24test2=test2"
 
     result2 = pylandax.Client.generate_url(base_url, {})
-    assert result2 == 'https://test.landax.com'
+    assert result2 == "https://test.landax.com"
